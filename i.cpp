9adab6abf17110b55a4070531975ea61e7e59527
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-31 10:31:50 -0400

    Add files via upload

---
+++ i.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//Main function
+int main()
+{
+char ch = 'a';
+cout <<"Alphabets from a - z are:"<< endl;
+while (ch<='z')
+{
+cout<< ch << endl ;
+ch++;
+}
+return 0;
+}
